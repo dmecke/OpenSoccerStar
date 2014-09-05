70ef5317c1d9a382b80eb4a957b0bb2136a50438
Daniel Mecke <daniel.mecke@cunningsoft.de>
2014-09-05 11:53:32 +0200

    add game date to menu bar

---
+++ src/OSS/CoreBundle/Twig/GameDateExtension.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace OSS\CoreBundle\Twig;
+
+use Doctrine\ORM\EntityManager;
+use OSS\CoreBundle\Entity\GameDate;
+
+class GameDateExtension extends \Twig_Extension
+{
+    /**
+     * @var EntityManager
+     */
+    private $entityManager;
+
+    /**
+     * @param EntityManager $entityManager
+     */
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @return array
+     */
+    public function getGlobals()
+    {
+        return array(
+            'gameDate' => $this->entityManager->getRepository('CoreBundle:GameDate')->findOneBy(array()),
+        );
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'game_date';
+    }
+}
